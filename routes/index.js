aa237f145bbc3bca3744fdfe9843011e83e6a247
Aarush Kumbhakern <aarushbeleren11@gmail.com>
2023-03-03 11:03:35 +0530

    INITIAL COMMIT

---
+++ routes/index.js
@@ -0,0 +1,20 @@
+var express = require("express");
+var router = express.Router();
+
+router.get("/", function(req, res, next) {
+	res.render("index", { title: "Home" });
+});
+
+router.get("/explore", function(req, res, next) {
+	res.render("explore", { title: "Explore" });
+});
+
+router.get("/about", function(req, res, next) {
+	res.render("about", { title: "About" });
+});
+
+router.get("/contact", function(req, res, next) {
+	res.render("contact", { title: "Contact" });
+});
+
+module.exports = router;
