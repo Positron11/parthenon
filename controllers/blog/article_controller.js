b2e15b6030f0ff2795de4c401c68ad42f84ad490
Aarush Kumbhakern <aarushbeleren11@gmail.com>
2023-03-07 23:14:39 +0530

    CLEAN UP
    Add comments and fix some error handling

--- controllers/blog/article_controller.js
+++ controllers/blog/article_controller.js
@@ -2,13 +2,17 @@ const { body, validationResult } = require("express-validator");
 
 const Article = require("../../models/blog/article");
 
+// article create view get method controller
 exports.article_create_get = (req, res, next) => {
+	// render article editor view
 	res.render("blog/article_editor", { 
 		title: "Create Article"
 	});
 }
 
+// article create view post method controller
 exports.article_create_post = [
+	// form validation
 	body("title", "Invalid title.").trim().isLength({ min: 1, max: 40 }).escape(),
 	body("subtitle", "Invalid subtitle.").trim().isLength({ min: 1, max: 40 }).escape(),
 	body("description", "Invalid description.").trim().isLength({ min: 1, max: 300 }).escape(), 
@@ -17,6 +21,7 @@ exports.article_create_post = [
 	(req, res, next) => {
 		const errors = validationResult(req);
 
+		// if errors, re-render form with data
 		if (!errors.isEmpty()) {
 			res.render("blog/article_editor", {
 				title: "Create Article",
@@ -25,6 +30,7 @@ exports.article_create_post = [
 			}); return;
 		}
 
+		// create new article instance
 		const article = new Article({ 
 			title: req.body.title,
 			subtitle: req.body.subtitle,
@@ -32,6 +38,7 @@ exports.article_create_post = [
 			content: req.body.content
 		});
 		
+		// save new article instance
 		article.save().then(
 			result => { res.redirect(result.url); }, 
 			err => { return next(err); }
@@ -39,15 +46,19 @@ exports.article_create_post = [
 	}
 ]
 
+// article delete view get method controller
 exports.article_delete_get = (req, res, next) => {
+	// get article by unique slug
 	Article.findOne({ slug: req.params.slug }).then(
 		result => {
+			// if article not found
 			if (result === null) {
 				const error = new Error("This article does not exist");
 				error.status = 404;
 				return next(error);
 			}
 
+			// render article delete view
 			res.render("blog/article_delete", {
 				title: "Delete Article",
 				article: result
@@ -57,22 +68,38 @@ exports.article_delete_get = (req, res, next) => {
 	);
 }
 
+// article delete view post method controller
 exports.article_delete_post = (req, res, next) => {
+	// get article by unique slug and delete it
 	Article.findOneAndDelete({ slug: req.params.slug }).then(
-		result => {	res.redirect("/blog") },
-		err => { return next(err); }
-	);
-}
-
-exports.article_update_get = (req, res, next) => {
-	Article.findOne({ slug: req.params.slug }).then(
-		result => {
+		result => {	
+			// if article does not exist
 			if (result === null) {
 				const error = new Error("This article does not exist");
 				error.status = 404;
 				return next(error);
 			}
 
+			// go back to article list view
+			res.redirect("/blog") 
+		},
+		err => { return next(err); }
+	);
+}
+
+// article update view get method controller
+exports.article_update_get = (req, res, next) => {
+	// get article by unique slug
+	Article.findOne({ slug: req.params.slug }).then(
+		result => {
+			// if article not found
+			if (result === null) {
+				const error = new Error("This article does not exist");
+				error.status = 404;
+				return next(error);
+			}
+
+			// render article editor view
 			res.render("blog/article_editor", { 
 				title: "Edit Article",
 				form_data: result
@@ -82,7 +109,9 @@ exports.article_update_get = (req, res, next) => {
 	);
 }
 
+// article update view post method controller
 exports.article_update_post = [
+	// form validation
 	body("title", "Invalid title.").trim().isLength({ min: 1, max: 40 }).escape(),
 	body("subtitle", "Invalid subtitle.").trim().isLength({ min: 1, max: 40 }).escape(),
 	body("description", "Invalid description.").trim().isLength({ min: 1, max: 300 }).escape(), 
@@ -91,6 +120,7 @@ exports.article_update_post = [
 	(req, res, next) => {
 		const errors = validationResult(req);
 
+		// if errors, re-render form with data
 		if (!errors.isEmpty()) {
 			res.render("blog/article_editor", {
 				title: "Create Article",
@@ -101,17 +131,20 @@ exports.article_update_post = [
 
 		Article.findOne({ slug: req.params.slug }).then(
 			result => { 
+				// if article does not exist
 				if (result === null) {
 					const error = new Error("This article does not exist");
 					error.status = 404;
 					return next(error);
 				}
 				
+				// update all fields
 				result.title = req.body.title;
 				result.subtitle = req.body.subtitle;
 				result.description = req.body.description;
 				result.content = req.body.content;
 
+				// save article and go to article url
 				result.save().then(
 					result => { res.redirect(result.url); },
 					err => { return next(err); }
