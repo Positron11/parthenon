7588dc8f9b606d879ae282d1860828c39f771b15
Aarush Kumbhakern <aarushbeleren11@gmail.com>
2023-03-07 22:45:08 +0530

    CLEAN UP
    Add comments, use const for require

--- routes/blog.js
+++ routes/blog.js
@@ -1,20 +1,27 @@
-var express = require("express");
+const express = require("express");
+
 var router = express.Router();
 
-var blog_controller = require("../controllers/blog/blog_controller")
-var article_controller = require("../controllers/blog/article_controller")
+// import controllers
+const blog_controller = require("../controllers/blog/blog_controller")
+const article_controller = require("../controllers/blog/article_controller")
 
+// article list view
 router.get("/", blog_controller.article_list);
 
+// article create views
 router.get("/create", article_controller.article_create_get);
 router.post("/create", article_controller.article_create_post);
 
+// article delete views
 router.get("/article/:slug/delete", article_controller.article_delete_get);
 router.post("/article/:slug/delete", article_controller.article_delete_post);
 
+// article edit views
 router.get("/article/:slug/edit", article_controller.article_update_get);
 router.post("/article/:slug/edit", article_controller.article_update_post);
 
+// article detail view
 router.get("/article/:slug", blog_controller.article_detail);
 
 module.exports = router;
