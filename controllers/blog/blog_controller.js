7cffdaea4ab99a2b3d4ba6c5a1780df7037e761f
Aarush Kumbhakern <aarushbeleren11@gmail.com>
2023-03-07 19:22:37 +0530

    IMPLEMENT ARTICLE  LIST AND DETAIL CONTROLLERS

--- controllers/blog/blog_controller.js
+++ controllers/blog/blog_controller.js
@@ -1,7 +1,31 @@
+const Article = require("../../models/blog/article");
+
 exports.article_list = (req, res, next) => {
-	res.send("NOT IMPLEMENTED: Article list.")
+	Article.find().sort({ updated_date: -1 }).then(
+		result => {
+			res.render("blog/index", { 
+				title: "Blog",
+				articles: result	
+			}); 
+		}, 
+		err => { return next(err); }
+	);
 }
 
 exports.article_detail = (req, res, next) => {
-	res.send("NOT IMPLEMENTED: Article detail.")
+	Article.findOne({ slug: req.params.slug }).then(
+		result => {
+			if (result === null) {
+				const error = new Error("This article does not exist");
+				error.status = 404;
+				return next(error);
+			}
+
+			res.render("blog/article", { 
+				title: result.title,
+				article: result	
+			}); 
+		}, 
+		err => { return next(err); }
+	);
 }
