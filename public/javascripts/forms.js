e35328badfd5f2c4827674fe2ea9df1d46ccf354
Aarush Kumbhakern <aarushbeleren11@gmail.com>
2023-04-17 03:47:01 +0530

    FIX FORM MULTIPLE SUBMISSION

--- public/javascripts/forms.js
+++ public/javascripts/forms.js
@@ -1,3 +1,8 @@
+document.addEventListener("submit", function(e) {
+	e.target.submit(function() { return false; })
+	e.target.querySelector("button[type='submit']").disabled = true; 
+}, true);
+
 document.addEventListener("invalid", function(e) {
 	e.target.classList.add("invalid");
 }, true);
