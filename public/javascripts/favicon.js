a63910f052ef00616fa7609cb2ab47b42142f80b
Aarush Kumbhakern <aarushbeleren11@gmail.com>
2023-05-07 14:47:17 +0530

    RE-ADD FAVICON

--- public/javascripts/favicon.js
+++
@@ -1,7 +0,0 @@
-// initialize favicon
-document.querySelector("link[rel~='icon']").href = `/images/site/favicon-${window.matchMedia('(prefers-color-scheme: dark)').matches ? "light" : "dark"}.svg`;
-
-// on change dark mode preference
-window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', event => { 
-	document.querySelector("link[rel~='icon']").href = `/images/site/favicon-${event.matches ? "light" : "dark"}.svg`; 
-});
--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,5 +1,18 @@
+// initialize favicon
+document.querySelector("link[rel~='icon']").href = `/images/site/favicon-${window.matchMedia('(prefers-color-scheme: dark)').matches ? "light" : "dark"}.svg`;
+
+// on change dark mode preference
+window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', event => { 
+	document.querySelector("link[rel~='icon']").href = `/images/site/favicon-${event.matches ? "light" : "dark"}.svg`; 
+});
+
 // computed dimensions class function
 Array.from(document.getElementsByClassName("uCalcDimensions")).forEach(function(element) {
+	calcDimensions(element);
+});
+
+// compute element dimensions and store in css variable
+function calcDimensions(element) {
 	// get element dimensions
 	element.style.setProperty("--height", `${element.clientHeight}px`)
 	element.style.setProperty("--width", `${element.clientWidth}px`)
@@ -13,4 +26,4 @@ Array.from(document.getElementsByClassName("uCalcDimensions")).forEach(function(
 	});
 	element.style.setProperty("--content-height", `${childHeightSum}px`)
 	element.style.setProperty("--content-width", `${childWidthSum}px`)
-});
+}
