b5b72a57a7c5680e1093d5b9620c08a5ce4570f8
Aarush Kumbhakern <aarushbeleren11@gmail.com>
2023-05-09 15:45:17 +0530

    IMPLEMENT DARK MODE

--- public/javascripts/main-loaded.js
+++ public/javascripts/main-loaded.js
@@ -1,12 +1,4 @@
-// initialize favicon
-document.querySelector("link[rel~='icon']").href = `/images/site/favicon-${window.matchMedia('(prefers-color-scheme: dark)').matches ? "light" : "dark"}.svg`;
-
-// on change dark mode preference
-window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', event => { 
-	document.querySelector("link[rel~='icon']").href = `/images/site/favicon-${event.matches ? "light" : "dark"}.svg`; 
-});
-
-// computed dimensions class function
+// apply computed dimensions class function
 Array.from(document.getElementsByClassName("uCalcDimensions")).forEach(function(element) {
 	calcDimensions(element);
 });
---
+++ public/javascripts/main-preload.js
@@ -0,0 +1,23 @@
+// initialize dark mode
+toggleDarkMode(localStorage.getItem("dark-mode") == "true");
+
+// initialize favicon
+document.querySelector("link[rel~='icon']").href = `/images/site/favicon-${window.matchMedia('(prefers-color-scheme: dark)').matches ? "light" : "dark"}.svg`;
+
+// change favicon style on change system dark mode preference
+window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', event => { 
+	document.querySelector("link[rel~='icon']").href = `/images/site/favicon-${event.matches ? "light" : "dark"}.svg`; 
+});
+
+// toggle dark mode
+function toggleDarkMode(state) {
+	// set local storage value
+	localStorage.setItem("dark-mode", state);
+	// temporarily disable all transitions
+	document.documentElement.classList.add("sChangingColorScheme");
+	// toggle dark mode
+	document.documentElement.classList.toggle("sDarkMode", state);
+	// force reflow and re-enable transitions
+	document.documentElement.offsetHeight;
+	document.documentElement.classList.remove("sChangingColorScheme");
+}
