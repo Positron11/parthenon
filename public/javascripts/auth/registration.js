20ff09244ccd0402569a65da940ce26e1f6f8634
Positron11 <aarushbeleren11@gmail.com>
2023-06-30 15:43:39 +0530

    FIX CAPTCHA MULTIPLICATION TOKEN

--- public/javascripts/auth/registration.js
+++ public/javascripts/auth/registration.js
@@ -3,7 +3,7 @@ const captchaField = document.getElementById("RegistrationFormCaptchaField");
 const captchaQuestion = document.getElementById("RegistrationFormCaptchaQuestion");
 
 // generate captcha string
-const operators = ["+", "-", "×"];
+const operators = ["+", "-", "*"];
 const x = Math.floor(Math.random() * 10);
 const y = Math.floor(Math.random() * 10);
 const operator = operators[Math.floor(Math.random() * operators.length)];
@@ -11,4 +11,4 @@ const captchaString = `${x} ${operator} ${y}`;
 
 // set captcha field attribute
 captchaQuestion.setAttribute("value", captchaString);
-captchaField.setAttribute("placeholder", `Solve: ${captchaString}`);
+captchaField.setAttribute("placeholder", `Solve: ${captchaString.replace("*", "×")}`);
