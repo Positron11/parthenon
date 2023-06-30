7857ff4447adc4fcbef6ff333f0831af76f7d6ee
Positron11 <aarushbeleren11@gmail.com>
2023-06-30 11:58:55 +0530

    FLASH MESSAGE ON LOGOUT

--- controllers/auth_controller.js
+++ controllers/auth_controller.js
@@ -8,12 +8,13 @@ exports.login_get = (req, res, next) => {
 exports.login_post = (req, res, next) => {
 	const returnTo = req.session.returnTo;
 	req.session.returnTo = undefined;
-	req.flash("success", `Welcome, ${req.user.username}`);
+	req.flash("info", `Welcome, ${req.user.username}`);
 	res.redirect(returnTo || "/");
 }
 
 // logout view controller
 exports.logout = (req, res, next) => {
+	req.flash("info", `Until next time, ${req.user.username}`);
 	req.logout();
 	res.redirect(req.query.next);
 }
