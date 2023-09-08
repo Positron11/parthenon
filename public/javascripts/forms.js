63fb546d2e48ae7ae4b1f8a6f0949500c9177d46
Positron11 <aarushbeleren11@gmail.com>
2023-09-08 20:01:00 +0530

    CREATE GENERIC PATTERN MATCH VALIDATION MECHANISM

--- public/javascripts/forms.js
+++ public/javascripts/forms.js
@@ -1,3 +1,13 @@
+// initialize submit buttons that require pattern match
+Array.from(document.querySelectorAll(".jPatternMatchRequired")).forEach(function(element) {	element.disabled = true; });
+
+// form submission confirmation
+Array.from(document.querySelectorAll(".jPatternInput")).forEach(function(element) {
+	element.addEventListener("input", function() { 
+		document.getElementById(element.dataset.for).disabled = !(element.value === element.pattern); 
+	});
+});
+
 // prevent form double submission on multiple submit button clicks
 document.addEventListener("submit", function(e) {
 	e.target.submit(function() { return false; })
